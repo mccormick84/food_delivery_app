db33b7fbcccc54d22cae926cb903438c39e8f3c0
Kenny <mccormick84@gmail.com>
2022-02-20 16:36:08 +0900

    app.tsx 초기 설정

--- android/app/src/main/java/com/fooddeliveryapp/MainActivity.java
+++ android/app/src/main/java/com/fooddeliveryapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.fooddeliveryapp;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +13,10 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "FoodDeliveryApp";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
